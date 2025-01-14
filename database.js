8b05074edc83eba71161e5ab339bf1f3344da2bd
esitorsunil <sunilrio23@gmail.com>
2025-01-14 18:42:18 +0530

    connect a database in node.js

---
+++ database.js
@@ -0,0 +1,28 @@
+const { MongoClient } = require('mongodb');
+
+
+const url = 
+   "mongodb+srv://maesitorsunil:hHCxW3Bj02xxj0H9@namastenode.h0eeh.mongodb.net/";
+
+   
+   const client = new MongoClient(url);
+   
+   // Database Name
+   const dbName = 'Hello';
+   
+   async function main() {
+     // Use connect method to connect to the server
+     await client.connect();
+     console.log('Connected successfully to server');
+     const db = client.db(dbName);
+     const collection = db.collection('User');
+   
+     // the following code examples can be pasted here...
+   
+     return 'done.';
+   }
+   
+   main()
+     .then(console.log)
+     .catch(console.error)
+     .finally(() => client.close());
